eb5936412cd307daeba056fa576d6a670e6b8846
Hannah Stampke <hannah.stampke@gmail.com>
2022-10-05 11:09:53 +1030

    added the js functions and started 5 day forecast

--- assets/script.js
+++ assets/script.js
@@ -28,3 +28,35 @@ searchBtn.addEventListener('click', function() {
 })
 
 // Connect search to API
+function displayCityData(data) {
+    var searchCityName = data.name;
+    var cityTemperature = data.main.temperature;
+    var cityWindSpeed = data.wind.speed;
+    var cityHumidity = data.main.humidity;
+    var longitude = data.coord.lon;
+    var latitude = data.coord.lat;
+
+    cityName.innerHTML = searchCityName;
+    temperature.innerHTML = cityTemperature + "°C";
+    windSpeed.innerHTML = cityWindSpeed + "km/h";
+    humidity.innerHTML = cityHumidity + "%";
+    dayOfWeek.innerHTML = todaysDate;
+
+    var cityForecastIcon = document.createElement("img");
+    cityForecastIcon.setAttribute("src", "https://openweathermap.org/img/wn/" + data.weather[0].icon + "@2x.png");
+    forecastIcon.appendChild(cityForecastIcon);
+
+    getFiveDayCity(latitude, longitude);
+    getUVIndex(latitude, longitude);
+}
+
+// Five day forecast from API
+function getFiveDayCity(latitude, longitude) {
+    let apiURL = "https://api.openweathermap.org/data/2.5/onecall?lat=" + latitude + "&lon=" + longitude + "&exclude=current,minutely,hourly,alerts&units=metric&appid=" + apiKey
+    fetch(apiURL)
+        .then(function(response) {
+            response.json().then(function(data) {
+                getFiveDayForecast(data);
+            })
+        });
+}
